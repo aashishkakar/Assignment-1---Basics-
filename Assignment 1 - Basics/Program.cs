caed659a33334d5eaae58834f176f702e874fa3e
aashishkakar@outlook.com <aashishkakar@outlook.com>
2019-01-27 14:52:28 -0500

    Add project files.

---
+++ Assignment 1 - Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_1___Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
